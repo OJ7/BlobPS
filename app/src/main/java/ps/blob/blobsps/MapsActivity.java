c7d6b7443515f2ba19bbbc6185003bb321160724
OJ7 <omairj7@gmail.com>
2014-11-25 01:26:14 -0500

    Fix sdk errors, add simple FAB, change initial loc
    FAB = floating action button

--- app/src/main/java/ps/blob/blobsps/MapsActivity.java
+++ app/src/main/java/ps/blob/blobsps/MapsActivity.java
@@ -1,8 +1,14 @@
 package ps.blob.blobsps;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -11,12 +17,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private FloatingActionButton fabButton;
+    private final LatLng UMD = new LatLng(38.989822, -76.940637);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        setupFAB();
     }
 
     @Override
@@ -24,6 +34,31 @@ public class MapsActivity extends FragmentActivity {
         super.onResume();
         setUpMapIfNeeded();
     }
+
+    /**
+     * Sets up the Floating Action Button the Map Screen
+     */
+    private void setupFAB() {
+        fabButton = new FloatingActionButton.Builder(this)
+                .withDrawable(
+                        getResources().getDrawable(R.drawable.ic_action_star))
+                .withButtonColor(Color.RED)
+                .withGravity(Gravity.BOTTOM | Gravity.RIGHT)
+                .withMargins(0, 0, 16, 16).create();
+        fabButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO - add more buttons when clicked
+                // TODO - implement material design animations
+                Intent intent = new Intent(MapsActivity.this,
+                        FloatingActionButton.class);
+                //startActivity(intent);
+            }
+        });
+
+    }
+
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -55,11 +90,16 @@ public class MapsActivity extends FragmentActivity {
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
+     * just add a marker near UMD.
      * <p>
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(38.986918,-76.942554)).title("Marker"));
+        // Centering Map on UMD
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UMD, 15));
+        // Disabling Zoom Buttons
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
     }
 }
