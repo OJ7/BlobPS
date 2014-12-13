f7622225ca9c4289eea30b82bae576cc142660ff
OJ7 <omairj7@gmail.com>
2014-12-13 16:58:18 -0500

    Fixed FAB buttons still being clickable when hidden

--- src/ps/blob/blobps/android/MapsActivity.java
+++ src/ps/blob/blobps/android/MapsActivity.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Toast;
 
@@ -32,8 +33,7 @@ public class MapsActivity extends FragmentActivity {
 	private GoogleMap mMap; // Might be null if Google Play services APK is not
 							// available.
 
-	private FloatingActionButton mainFAB, locationFAB, blobFAB, itemFAB,
-			combineFAB;
+	private FloatingActionButton mainFAB, locationFAB, blobFAB, itemFAB, combineFAB;
 	private int expandFAB = 0; // 0 = collapsed, 1 = expanded
 	private int locToggle = 0; // 0 = center on current location, 1 = center on
 								// map
@@ -41,10 +41,9 @@ public class MapsActivity extends FragmentActivity {
 	private final LatLng UMD = new LatLng(38.986918, -76.942554);
 	private LatLng myLocation = UMD;
 
-	private final double NORTH = 39.001460, EAST = -76.956008,
-			SOUTH = 38.980446, WEST = -76.931203;
-	private final LatLng UMD_NE = new LatLng(NORTH, EAST), UMD_SW = new LatLng(
-			SOUTH, WEST);
+	private final double NORTH = 39.001460, EAST = -76.956008, SOUTH = 38.980446,
+			WEST = -76.931203;
+	private final LatLng UMD_NE = new LatLng(NORTH, EAST), UMD_SW = new LatLng(SOUTH, WEST);
 	private final LatLngBounds UMD_BOUNDS = new LatLngBounds(UMD_SW, UMD_NE);
 
 	private final int numAreas = 8; // grid will be numAreas x numAreas
@@ -76,11 +75,11 @@ public class MapsActivity extends FragmentActivity {
 	}
 
 	/**
-	 * Hides location button and shows menu item buttons like this: (Combine)
-	 * (Items) (Blobs) (X)
+	 * Hides location button and shows menu item buttons like this: (Combine) (Items) (Blobs) (X)
 	 */
 	private void expandFABMenu() {
-		locationFAB.hideFloatingActionButton();
+		ViewGroup vg = (ViewGroup) (locationFAB.getParent());
+		vg.removeView(locationFAB);
 
 		blobFABListener();
 		itemFABListener();
@@ -91,9 +90,10 @@ public class MapsActivity extends FragmentActivity {
 	 * Hides menu item buttons and shows buttons like this: (Location) (Menu)
 	 */
 	private void contractFABMenu() {
-		blobFAB.hideFloatingActionButton();
-		combineFAB.hideFloatingActionButton();
-		itemFAB.hideFloatingActionButton();
+		ViewGroup vg = (ViewGroup) (blobFAB.getParent());
+		vg.removeView(blobFAB);
+		vg.removeView(itemFAB);
+		vg.removeView(combineFAB);
 
 		locationFABListener();
 	}
@@ -101,10 +101,8 @@ public class MapsActivity extends FragmentActivity {
 	private void mainFABListener() {
 		// Creates Floating Action Button for Menu
 		mainFAB = new FloatingActionButton.Builder(this)
-				.withDrawable(
-						getResources().getDrawable(R.drawable.ic_action_star))
-				.withButtonColor(Color.RED)
-				.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
+				.withDrawable(getResources().getDrawable(R.drawable.ic_action_star))
+				.withButtonColor(Color.RED).withGravity(Gravity.BOTTOM | Gravity.RIGHT)
 				.withMargins(0, 0, 16, 16).create();
 
 		// When clicked, menu will expand or collapse
@@ -114,13 +112,13 @@ public class MapsActivity extends FragmentActivity {
 				if (expandFAB == 0) {
 					expandFAB = 1;
 					expandFABMenu();
-					mainFAB.setFloatingActionButtonDrawable(getResources()
-							.getDrawable(R.drawable.ic_action_cancel));
+					mainFAB.setFloatingActionButtonDrawable(getResources().getDrawable(
+							R.drawable.ic_action_cancel));
 				} else {
 					expandFAB = 0;
 					contractFABMenu();
-					mainFAB.setFloatingActionButtonDrawable(getResources()
-							.getDrawable(R.drawable.ic_action_star));
+					mainFAB.setFloatingActionButtonDrawable(getResources().getDrawable(
+							R.drawable.ic_action_star));
 				}
 			}
 		});
@@ -131,22 +129,16 @@ public class MapsActivity extends FragmentActivity {
 		if (locToggle == 0) {
 			// Current Location
 			locationFAB = new FloatingActionButton.Builder(this)
-					.withDrawable(
-							getResources().getDrawable(
-									R.drawable.ic_action_locate))
+					.withDrawable(getResources().getDrawable(R.drawable.ic_action_locate))
 					.withButtonColor(Color.parseColor("#00A0B0"))
-					.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-					.withMargins(0, 0, 16, 86).create();
+					.withGravity(Gravity.BOTTOM | Gravity.RIGHT).withMargins(0, 0, 16, 86).create();
 
 		} else {
 			// Campus Location
 			locationFAB = new FloatingActionButton.Builder(this)
-					.withDrawable(
-							getResources().getDrawable(
-									R.drawable.ic_action_locate))
+					.withDrawable(getResources().getDrawable(R.drawable.ic_action_locate))
 					.withButtonColor(Color.parseColor("#BD1550"))
-					.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-					.withMargins(0, 0, 16, 86).create();
+					.withGravity(Gravity.BOTTOM | Gravity.RIGHT).withMargins(0, 0, 16, 86).create();
 
 		}
 
@@ -159,20 +151,16 @@ public class MapsActivity extends FragmentActivity {
 			public void onClick(View v) {
 				if (locToggle == 0) {
 					locToggle = 1;
-					locationFAB.setFloatingActionButtonColor(Color
-							.parseColor("#BD1550"));
+					locationFAB.setFloatingActionButtonColor(Color.parseColor("#BD1550"));
 					centerMapOnMyLocation();
-					Toast.makeText(getApplicationContext(),
-							"Centering map on current location",
+					Toast.makeText(getApplicationContext(), "Centering map on current location",
 							Toast.LENGTH_SHORT).show();
 				} else {
 					locToggle = 0;
-					locationFAB.setFloatingActionButtonColor(Color
-							.parseColor("#00A0B0"));
+					locationFAB.setFloatingActionButtonColor(Color.parseColor("#00A0B0"));
 					centerMapOnCampus();
-					Toast.makeText(getApplicationContext(),
-							"Centering map on campus", Toast.LENGTH_SHORT)
-							.show();
+					Toast.makeText(getApplicationContext(), "Centering map on campus",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
@@ -181,11 +169,9 @@ public class MapsActivity extends FragmentActivity {
 	private void blobFABListener() {
 		// Creates Floating Action Button for Blobs List
 		blobFAB = new FloatingActionButton.Builder(this)
-				.withDrawable(
-						getResources().getDrawable(R.drawable.ic_launcher))
+				.withDrawable(getResources().getDrawable(R.drawable.ic_launcher))
 				.withButtonColor(Color.parseColor("#EDC951"))
-				.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-				.withMargins(0, 0, 16, 86).create();
+				.withGravity(Gravity.BOTTOM | Gravity.RIGHT).withMargins(0, 0, 16, 86).create();
 
 		blobFAB.hideFloatingActionButton();
 		blobFAB.showFloatingActionButton();
@@ -194,9 +180,8 @@ public class MapsActivity extends FragmentActivity {
 		blobFAB.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(),
-						"Unimplemented - Blobs Listing", Toast.LENGTH_SHORT)
-						.show();
+				Toast.makeText(getApplicationContext(), "Unimplemented - Blobs Listing",
+						Toast.LENGTH_SHORT).show();
 			}
 		});
 	} // end of blobFABListener
@@ -204,11 +189,9 @@ public class MapsActivity extends FragmentActivity {
 	private void itemFABListener() {
 		// Creates Floating Action Button for Item List
 		itemFAB = new FloatingActionButton.Builder(this)
-				.withDrawable(
-						getResources().getDrawable(R.drawable.ic_launcher))
+				.withDrawable(getResources().getDrawable(R.drawable.ic_launcher))
 				.withButtonColor(Color.parseColor("#CBE86B"))
-				.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-				.withMargins(0, 0, 16, 156).create();
+				.withGravity(Gravity.BOTTOM | Gravity.RIGHT).withMargins(0, 0, 16, 156).create();
 
 		itemFAB.hideFloatingActionButton();
 		itemFAB.showFloatingActionButton();
@@ -217,9 +200,8 @@ public class MapsActivity extends FragmentActivity {
 		itemFAB.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(),
-						"Unimplemented - Item Listing", Toast.LENGTH_SHORT)
-						.show();
+				Toast.makeText(getApplicationContext(), "Unimplemented - Item Listing",
+						Toast.LENGTH_SHORT).show();
 			}
 
 		});
@@ -228,11 +210,9 @@ public class MapsActivity extends FragmentActivity {
 	private void combineFABListener() {
 		// Creates Floating Action Button for Combine Activity
 		combineFAB = new FloatingActionButton.Builder(this)
-				.withDrawable(
-						getResources().getDrawable(R.drawable.ic_launcher))
+				.withDrawable(getResources().getDrawable(R.drawable.ic_launcher))
 				.withButtonColor(Color.parseColor("#53777A"))
-				.withGravity(Gravity.BOTTOM | Gravity.RIGHT)
-				.withMargins(0, 0, 16, 226).create();
+				.withGravity(Gravity.BOTTOM | Gravity.RIGHT).withMargins(0, 0, 16, 226).create();
 
 		combineFAB.hideFloatingActionButton();
 		combineFAB.showFloatingActionButton();
@@ -241,13 +221,11 @@ public class MapsActivity extends FragmentActivity {
 		combineFAB.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(),
-						"Implement Combine Screen Activity", Toast.LENGTH_SHORT)
-						.show();
+				Toast.makeText(getApplicationContext(), "Implement Combine Screen Activity",
+						Toast.LENGTH_SHORT).show();
 				// TODO - launch combine activity
 
-				Intent intent = new Intent(MapsActivity.this,
-						CombineActivity.class);
+				Intent intent = new Intent(MapsActivity.this, CombineActivity.class);
 				startActivity(intent);
 
 			}
@@ -256,21 +234,18 @@ public class MapsActivity extends FragmentActivity {
 	} // end of combineFABListener
 
 	/**
-	 * Sets up the map if it is possible to do so (i.e., the Google Play
-	 * services APK is correctly installed) and the map has not already been
-	 * instantiated.. This will ensure that we only ever call
-	 * {@link #setUpMap()} once when {@link #mMap} is not null.
+	 * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+	 * installed) and the map has not already been instantiated.. This will ensure that we only ever
+	 * call {@link #setUpMap()} once when {@link #mMap} is not null.
 	 * <p/>
 	 * If it isn't installed {@link SupportMapFragment} (and
-	 * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt
-	 * for the user to install/update the Google Play services APK on their
-	 * device.
+	 * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+	 * install/update the Google Play services APK on their device.
 	 * <p/>
-	 * A user can return to this FragmentActivity after following the prompt and
-	 * correctly installing/updating/enabling the Google Play services. Since
-	 * the FragmentActivity may not have been completely destroyed during this
-	 * process (it is likely that it would only be stopped or paused),
-	 * {@link #onCreate(Bundle)} may not be called again so we should call this
+	 * A user can return to this FragmentActivity after following the prompt and correctly
+	 * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+	 * have been completely destroyed during this process (it is likely that it would only be
+	 * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
 	 * method in {@link #onResume()} to guarantee that it will be called.
 	 */
 	private void setUpMapIfNeeded() {
@@ -278,8 +253,8 @@ public class MapsActivity extends FragmentActivity {
 		// map.
 		if (mMap == null) {
 			// Try to obtain the map from the SupportMapFragment.
-			mMap = ((SupportMapFragment) getSupportFragmentManager()
-					.findFragmentById(R.id.map)).getMap();
+			mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+					.getMap();
 			// Check if we were successful in obtaining the map.
 			if (mMap != null) {
 				setUpMap();
@@ -288,11 +263,10 @@ public class MapsActivity extends FragmentActivity {
 	}
 
 	/**
-	 * This is where we can add markers or lines, add listeners or move the
-	 * camera. In this case, we just add a marker near UMD.
+	 * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+	 * just add a marker near UMD.
 	 * <p/>
-	 * This should only be called once and when we are sure that {@link #mMap}
-	 * is not null.
+	 * This should only be called once and when we are sure that {@link #mMap} is not null.
 	 */
 	private void setUpMap() {
 		// Centering Map on UMD
@@ -309,8 +283,8 @@ public class MapsActivity extends FragmentActivity {
 	}
 
 	/**
-	 * Centers map on current location. If current location can not be resolved,
-	 * it defaults to UMD location.
+	 * Centers map on current location. If current location can not be resolved, it defaults to UMD
+	 * location.
 	 */
 	private void centerMapOnMyLocation() {
 
@@ -319,8 +293,7 @@ public class MapsActivity extends FragmentActivity {
 		Location location = mMap.getMyLocation();
 
 		if (location != null) {
-			myLocation = new LatLng(location.getLatitude(),
-					location.getLongitude());
+			myLocation = new LatLng(location.getLatitude(), location.getLongitude());
 		}
 		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 17));
 	}
@@ -335,36 +308,31 @@ public class MapsActivity extends FragmentActivity {
 	/**
 	 * Makes a grid to overlay on top of UMD Campus.
 	 * 
-	 * Done by making a new AreaGrid for each cell inside {@link #grid) by
-	 * dividing {@link #UMD_BOUNDS} into equal parts by Latitude and Longitude
-	 * and adding an overlay from an image onto that AreaGrid.
+	 * Done by making a new AreaGrid for each cell inside {@link #grid) by dividing
+	 * {@link #UMD_BOUNDS} into equal parts by Latitude and Longitude and adding an overlay from an
+	 * image onto that AreaGrid.
 	 */
 	private void setUpGrid() {
-		double areaLength = Math.abs(EAST - WEST) / numAreas, areaHeight = Math
-				.abs(NORTH - SOUTH) / numAreas;
+		double areaLength = Math.abs(EAST - WEST) / numAreas, areaHeight = Math.abs(NORTH - SOUTH)
+				/ numAreas;
 		int areaID = 0;
 		for (int row = 0; row < numAreas; row++) {
 			for (int col = 0; col < numAreas; col++) {
-				LatLng SW = new LatLng(NORTH - (row + 1) * areaHeight, EAST
-						+ col * areaLength);
-				LatLng NE = new LatLng(NORTH - row * areaHeight, EAST
-						+ (col + 1) * areaLength);
+				LatLng SW = new LatLng(NORTH - (row + 1) * areaHeight, EAST + col * areaLength);
+				LatLng NE = new LatLng(NORTH - row * areaHeight, EAST + (col + 1) * areaLength);
 				Log.i("MapsActivity", "areaID = " + areaID);
 				Log.i("MapsActivity", "SW = " + SW.toString());
 				Log.i("MapsActivity", "NE = " + NE.toString());
 
 				AreaGrid area = new AreaGrid(SW, NE, areaID++);
 				GroundOverlayOptions areaOverlayOptions = new GroundOverlayOptions()
-						.image(BitmapDescriptorFactory
-								.fromResource(R.drawable.grey_overlay))
-						.transparency((float) 0.25)
-						.positionFromBounds(area.getAreaBounds());
+						.image(BitmapDescriptorFactory.fromResource(R.drawable.grey_overlay))
+						.transparency((float) 0.25).positionFromBounds(area.getAreaBounds());
 				if (areaID == 11) {
 					areaOverlayOptions.image(BitmapDescriptorFactory
 							.fromResource(R.drawable.event_overlay));
 				}
-				GroundOverlay areaOverlay = mMap
-						.addGroundOverlay(areaOverlayOptions);
+				GroundOverlay areaOverlay = mMap.addGroundOverlay(areaOverlayOptions);
 				area.setGroundOverlay(areaOverlay);
 				grid[row][col] = area;
 			}
@@ -372,12 +340,11 @@ public class MapsActivity extends FragmentActivity {
 	}
 
 	/**
-	 * NOTE: currently removes the grid overlays (instead of popup dialog) for
-	 * testing purposes
+	 * NOTE: currently removes the grid overlays (instead of popup dialog) for testing purposes
 	 * 
-	 * Calculates where on the map was clicked and brings up a popup box
-	 * displaying information of the area. If an area outside of campus is
-	 * clicked, a Toast message is displayed informing the user.
+	 * Calculates where on the map was clicked and brings up a popup box displaying information of
+	 * the area. If an area outside of campus is clicked, a Toast message is displayed informing the
+	 * user.
 	 */
 	private void handleClickOnMap() {
 		mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -395,17 +362,15 @@ public class MapsActivity extends FragmentActivity {
 				}
 
 				if (tappedArea != null) { // Area is inside grid
-					Toast.makeText(
-							getApplicationContext(),
-							"Tapped on area with ID: " + tappedArea.getAreaID(),
-							Toast.LENGTH_SHORT).show();
+					Toast.makeText(getApplicationContext(),
+							"Tapped on area with ID: " + tappedArea.getAreaID(), Toast.LENGTH_SHORT)
+							.show();
 					if (tappedArea.getGroundOverlay() != null) {
 						tappedArea.removeGroundOverlay();
 					}
 				} else { // Area is outside of grid
-					Toast.makeText(getApplicationContext(),
-							"Tapped outside of campus grid", Toast.LENGTH_SHORT)
-							.show();
+					Toast.makeText(getApplicationContext(), "Tapped outside of campus grid",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
